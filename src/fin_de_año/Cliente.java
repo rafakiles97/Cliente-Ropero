72468b3762b2441eca1044e426ffc175349da152
Natalia <n.entremonzagamonje@gmail.com>
2018-01-11 11:52:05 +0100

    anadir main y thread clase cliente

--- src/fin_de_año/Cliente.java
+++ src/fin_de_año/Cliente.java
@@ -1,11 +1,13 @@
 package fin_de_año;
 
-public class Cliente {
+public class Cliente extends Thread {
 
 	private boolean tieneAbrigo;
-	
-	public Cliente() {
+	private Ropero ropero = null;
+
+	public Cliente(Ropero ropero) {
 		this.tieneAbrigo = true;
+		this.ropero= ropero;
 	}
 
 	public boolean isTieneAbrigo() {
@@ -15,35 +17,49 @@ public class Cliente {
 	public void setTieneAbrigo(boolean tieneAbrigo) {
 		this.tieneAbrigo = tieneAbrigo;
 	}
-	
-	public void DejarAbrigo(Ropero ropero) throws Exception{
-		try {
-			Thread.sleep(5000);
-			if(isTieneAbrigo() == true){
-				ropero.LlenarInventario();
-				setTieneAbrigo(false);
-			}else{
-				throw new Exception("No has podido dejar el abrigo");
+
+	public void DejarAbrigo() throws Exception {
+		synchronized (ropero) {
+			try {
+				Thread.sleep(5000);
+				if (isTieneAbrigo() == true) {
+					ropero.LlenarInventario();
+					setTieneAbrigo(false);
+					System.out.println("has dejado el abrigo");
+				} else {
+					throw new Exception("No has podido dejar el abrigo");
+				}
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
+		}
 	}
-	
-	public void RecogerAbrigo(Ropero ropero) throws Exception{
+
+	public void RecogerAbrigo(Ropero ropero) throws Exception {
 		try {
 			Thread.sleep(5000);
-			if(isTieneAbrigo() == false){
+			if (isTieneAbrigo() == false) {
 				ropero.VaciarInventario();
 				setTieneAbrigo(true);
-			}else{
-				throw new Exception("No has podido recoger el abrigo, Ya tienes uno......");
+			} else {
+				throw new Exception(
+						"No has podido recoger el abrigo, Ya tienes uno......");
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void run() {
+		try {
+			DejarAbrigo();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
--- src/fin_de_año/Main.java
+++ src/fin_de_año/Main.java
@@ -2,14 +2,17 @@ package fin_de_año;
 
 public class Main {
 
-	Ropero ropero1 = new Ropero(2);
+	static Ropero ropero1 = new Ropero(2);
 	
-	Cliente cliente1 = new Cliente();
-	Cliente cliente2 = new Cliente();
-	Cliente cliente3 = new Cliente();
-	Cliente cliente4 = new Cliente();
+	static Cliente cliente1 = new Cliente(ropero1);
+	static Cliente cliente2 = new Cliente(ropero1);
+	static Cliente cliente3 = new Cliente(ropero1);
+	static Cliente cliente4 = new Cliente(ropero1);
 	
 	public static void main(String[] args) {
-		
+		cliente1.start();
+		cliente2.start();
+		cliente3.start();
+		cliente4.start();
 	}
 }
